55ac8c76fcfdfca787af406bd85c4cdcc39567bf
Joe Chambers <josephbrianchambers@gmail.com>
2019-03-18 16:02:15 -0700

    Support circleci testing
    - Add circle config files to support github testing integration
    - add minitest reporters gem (https://github.com/kern/minitest-reporters) to format test results in a circleci friendly way

---
+++ test/helpers/simple_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class SimpleTest < ActiveSupport::TestCase
+  test 'truthy result' do
+    assert_equal(true, true)
+  end
+
+  # test 'falsy result' do
+  #   assert_equal(true, false)
+  # end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,16 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require "minitest/reporters"
 
 class ActiveSupport::TestCase
+  Minitest::Reporters.use!(
+    [
+      Minitest::Reporters::DefaultReporter.new,
+      Minitest::Reporters::JUnitReporter.new("tmp/test_results/minitest")
+    ]
+  )
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
